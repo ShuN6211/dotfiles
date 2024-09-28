a9531be3b2683eff6c2e028fc875f799fe80e13e
Shun Makino <shunmakino6211@gmail.com>
2024-09-29 02:12:56 +0900

    emacs: add editorconfig

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -91,7 +91,13 @@
   (set-default-coding-systems 'utf-8)
   (set-terminal-coding-system 'utf-8)
   (set-keyboard-coding-system 'utf-8)
-(set-buffer-file-coding-system 'utf-8))
+  (set-buffer-file-coding-system 'utf-8))
+
+(leaf editorconfig
+  :ensure t
+  :require t
+  :config
+  (editorconfig-mode 1))
 
 ;; auto-convert
 (leaf autorevert
