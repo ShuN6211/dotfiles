874d911a65b3668b22b45751c6645b7536849eed
Shun Makino <shunmakino6211@gmail.com>
2024-12-18 02:33:16 +0900

    zsh: always show node version from nvm

--- config/zsh/.p10k.zsh
+++ config/zsh/.p10k.zsh
@@ -1026,11 +1026,11 @@
   typeset -g POWERLEVEL9K_NVM_FOREGROUND=70
   # If set to false, hide node version if it's the same as default:
   # $(nvm version current) == $(nvm version default).
-  typeset -g POWERLEVEL9K_NVM_PROMPT_ALWAYS_SHOW=false
+  typeset -g POWERLEVEL9K_NVM_PROMPT_ALWAYS_SHOW=true
   # If set to false, hide node version if it's equal to "system".
   typeset -g POWERLEVEL9K_NVM_SHOW_SYSTEM=true
   # Custom icon.
-  # typeset -g POWERLEVEL9K_NVM_VISUAL_IDENTIFIER_EXPANSION='⭐'
+  typeset -g POWERLEVEL9K_NVM_VISUAL_IDENTIFIER_EXPANSION='🚀'
 
   ############[ nodeenv: node.js environment (https://github.com/ekalinin/nodeenv) ]############
   # Nodeenv color.
