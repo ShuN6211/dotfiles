65212c683d7584335d65d9f029a84fe2248472f2
Shun Makino <shun.makino@mixi.co.jp>
2025-06-16 22:35:00 +0900

    finicky: add finicky config

---
+++ config/finicky/finicky.ts
@@ -0,0 +1,35 @@
+import type {
+    BrowserHandler,
+    FinickyConfig,
+} from "/Applications/Finicky.app/Contents/Resources/finicky.d.ts";
+
+// Google Meet / Sheets / Docs / Slides is opend in Google Chrome 
+const googleChromeOnlyHandler: BrowserHandler = {
+    match: (url: URL) => {
+        return (
+            url.host === "meet.google.com" ||
+            (url.host === "docs.google.com" &&
+                (url.pathname.startsWith("/spreadsheets") ||
+                    url.pathname.startsWith("/document") ||
+                    url.pathname.startsWith("/presentation")))
+        );
+    },
+    browser: "Google Chrome",
+};
+
+// otherwise, open in Firefox
+const fallbackHandler: BrowserHandler = {
+    match: () => true,
+    browser: "Firefox",
+};
+
+export default {
+    defaultBrowser: "Firefox",
+    options: {
+        checkForUpdates: false,
+    },
+    handlers: [
+        googleChromeOnlyHandler,
+        fallbackHandler,
+    ],
+} satisfies FinickyConfig;
